dec1da80687472cf4f8921ff5ca222baf12a716f
Ojietohamen Samuel <seekersoftec@gmail.com>
2024-01-15 17:59:37 +0100

    add  load model first technique

--- application/initializer.py
+++ application/initializer.py
@@ -1,6 +1,11 @@
+import tensorflow as tf
+from application.main.config import settings
+
+
 class LoggerInstance(object):
     def __new__(cls):
         from application.main.utility.logger.custom_logging import LogHandler
+
         return LogHandler()
 
 
@@ -8,9 +13,12 @@ class IncludeAPIRouter(object):
     def __new__(cls):
         # from application.main.routers.health_checks import router as router_health_check
         from application.main.routers.index import router as router_index
+
         # from application.main.routers.api_response import router as response_manager_test
         # from application.main.routers.question_classifier import router as router_question_classification
-        from application.main.routers.image_classifier import router as router_image_classification
+        from application.main.routers.image_classifier import (
+            router as router_image_classification,
+        )
         from fastapi.routing import APIRouter
 
         router = APIRouter()
@@ -18,18 +26,24 @@ class IncludeAPIRouter(object):
         # router.include_router(response_manager_test, prefix='/api/v1', tags=['response_manager'])
         # router.include_router(router_question_classification, prefix='/api/v1', tags=['question_classification'])
 
-        router.include_router(router_index, prefix='', tags=['index'])
-        router.include_router(router_image_classification, prefix='/api/v1', tags=['image_classification'])
-        
+        router.include_router(router_index, prefix="", tags=["index"])
+        router.include_router(
+            router_image_classification, prefix="/api/v1", tags=["image_classification"]
+        )
+
         return router
 
 
 class DataBaseInstance(object):
     def __new__(cls):
         from application.main.infrastructure.database import db
+
         return db.DataBase()
 
- 
+
 # instance creation
 logger_instance = LoggerInstance()
 db_instance = DataBaseInstance()
+
+# load model first to make it easy when using model again
+tf.keras.models.load_model(settings.APP_CONFIG.DR_CLASSIFICATION_MODEL)
